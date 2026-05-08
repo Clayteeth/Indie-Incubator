070499e747eaee96bc92de86ce8e54ab6cf50334
Clay <mussfeldt.casa@gmail.com>
2026-05-08 16:11:19 -0700

    Camera bug

--- Indie incubator/Assets/Coding/YouDied.cs
+++ Indie incubator/Assets/Coding/YouDied.cs
@@ -4,7 +4,14 @@ using System.Collections;
 public class EnemyKillPlayer : MonoBehaviour
 {
     public GameObject deathUI;       
-    public Transform respawnPoint;   
+    public Transform respawnPoint;
+    public Camera ClawCameraV;
+    public Camera GroundCameraV;
+    public float deathCutsceneTime;
+    public Animator ENEMY_COPY;
+    public PlayerMove playerScriptV;
+    public Camera playerCameraV;
+    public string Death_Start;
 
     private bool dead = false;
 
@@ -15,22 +22,39 @@ public class EnemyKillPlayer : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             dead = true;
+            playerScriptV.enabled = false;
+            playerCameraV.enabled = false;
+            ENEMY_COPY.SetTrigger(Death_Start);
             StartCoroutine(DeathRoutine(other));
+        
         }
     }
 
     IEnumerator DeathRoutine(Collider player)
     {
-        deathUI.SetActive(true);
+        
 
-        CharacterController cc = player.GetComponent<CharacterController>();
-        if (cc != null) cc.enabled = false;
+        //CharacterController cc = player.GetComponent<CharacterController>();
+        //if (cc != null) cc.enabled = false;
+        
+
+        GroundCameraV.enabled = true;
+        yield return new WaitForSeconds(17f);
+        GroundCameraV.enabled = false;
+        ClawCameraV.enabled = true;
+
+        yield return new WaitForSeconds(53f);
+        ClawCameraV.enabled = false;
+
+        deathUI.SetActive(true);
 
         yield return new WaitForSeconds(2f);
 
         player.transform.position = respawnPoint.position;
 
-        if (cc != null) cc.enabled = true;
+        //if (cc != null) cc.enabled = true;
+        playerScriptV.enabled = true;
+        playerCameraV.enabled = true;
 
         deathUI.SetActive(false);
 
